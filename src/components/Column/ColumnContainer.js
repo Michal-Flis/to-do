a6c03cd32ce2f69c7ee39174ee056f99ee13b6d3
Michał Flis <michal.flis@op.pl>
2021-10-18 17:43:08 +0200

    corected Container

--- src/components/Column/ColumnContainer.js
+++ src/components/Column/ColumnContainer.js
@@ -2,7 +2,6 @@ import {connect} from 'react-redux';
 import Column from './Column';
 import { createActionAddCard, getColumnsForColumn } from '../Redux/columnRedux';
 
-// export const getColumnsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
 
 const mapStateToProps = (state, props) => ({
   cards: getColumnsForColumn(state, props.id),
--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -2,7 +2,6 @@ import {connect} from 'react-redux';
 import List from './List';
 import { getColumnsForList, createActionAddColumn } from '../Redux/columnRedux';
 
-// export const getColumnsForList = ({columns}, listId) => columns.filter(column => column.listId == listId);
 
 const mapStateToProps = (state, props) => ({
   columns: getColumnsForList(state, props.id),
